7f9c1d7872061017eb0c260acec167db2c22c36e
fernando <fernando@juandemariana.org>
2024-12-27 23:35:13 +0100

    Fix Home controller for translations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use App\Models\MetaTag;
+use Illuminate\Support\Facades\App; 
+use Illuminate\Support\Facades\Lang;
+
+
+
 
 class HomeController extends Controller
 {
     public function index(): View
     {
+        //dd(Lang::get('messages.login'));
         $metaTags = MetaTag::where('route_name', 'home')->first();
+        App::setLocale('es'); // O usa \Illuminate\Support\Facades\App::setLocale('es');
+        
 
         return view('welcome', [
             'metaTags' => $metaTags,
         ]);
     }
-
 }
+
 
