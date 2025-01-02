276c7a3857a2f06b27b148ecb9aca6edacff30f3
fernando <fernando@juandemariana.org>
2025-01-02 23:34:41 +0100

    Add Video Filament resource

---
+++ app/Filament/Resources/VideoResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VideoResource\Pages;
+use App\Filament\Resources\VideoResource\RelationManagers;
+use App\Models\Video;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VideoResource extends Resource
+{
+    protected static ?string $model = Video::class;
+    protected static ?string $navigationGroup = 'Academic content';
+
+    protected static ?string $navigationIcon = 'iconoir-youtube';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->maxLength(255),
+                Forms\Components\Select::make('language')
+                    ->options([
+                        'en' => 'English',
+                        'es' => 'Español',
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('url')
+                    ->label('Video URL')
+                    ->url()
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('slug')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('language')->sortable(),
+                Tables\Columns\TextColumn::make('url')->label('Video URL'),
+                Tables\Columns\TextColumn::make('created_at')->dateTime(),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVideos::route('/'),
+            'create' => Pages\CreateVideo::route('/create'),
+            'edit' => Pages\EditVideo::route('/{record}/edit'),
+        ];
+    }
+}
