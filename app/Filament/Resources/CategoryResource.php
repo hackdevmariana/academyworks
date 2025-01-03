1269e1142537afd018880f801106ac04ed6c0437
fernando <fernando@juandemariana.org>
2025-01-03 21:10:19 +0100

    Add Category resource

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+    protected static ?string $navigationGroup = 'Academic content';
+
+    protected static ?string $navigationIcon = 'heroicon-s-tag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Name'),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->unique(Category::class, 'slug')
+                    ->maxLength(255)
+                    ->label('Slug'),
+            ]);
+    }
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Name'),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Slug'),
+                Tables\Columns\TextColumn::make('books_count')
+                    ->counts('books')
+                    ->label('Books'),
+                Tables\Columns\TextColumn::make('quotes_count')
+                    ->counts('quotes')
+                    ->label('Quotes'),
+            ])
+            ->filters([
+                // Agrega filtros si son necesarios
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
