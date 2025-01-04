fdb227bc2290bbe4c13d7eabc4983d1f62891b6f
fernando <fernando@juandemariana.org>
2025-01-04 21:55:51 +0100

    Install Jetstream

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
-<html lang="{{ $metaTags->lang ?? 'en' }}">
-<head>
-    <title>{{ $metaTags->title ?? 'Default Title' }}</title>
-    @include('partials.meta', ['metaTags' => $metaTags])
-    @include('partials.styles')
-</head>
-<body>
-    <div class="page-wraper">
-        {{-- @include('partials.header') --}}
-        @include('partials.navbar')
-        @include('partials.hero')
-        @include('partials.carousel-courses')
-        <main>
-            @yield('content')
-        </main>
-        @include('partials.carousel-events')
-        @include('partials.recommended-channels')
-        @include('partials.carousel-quotes')
-        
-        @include('partials.footer')
-    </div>
-    @include('partials.scripts')
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+        <x-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-menu')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ Route::get('/event/{slug}', [EventController::class, 'show'])->name('event.show'
 Route::get('/readings/{slug}', [ReadingController::class, 'show'])->name('readings.show');
 Route::get('/speakers', [SpeakerController::class, 'index'])->name('speakers.index');
 Route::get('/speakers/{id}', [SpeakerController::class, 'show'])->name('speakers.show');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
