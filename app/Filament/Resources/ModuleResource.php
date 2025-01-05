bb09f91097d671998bb96472d5d5726cf1824e92
fernando <fernando@juandemariana.org>
2025-01-05 08:12:47 +0100

    Add Module resource

---
+++ app/Filament/Resources/ModuleResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ModuleResource\Pages;
+use App\Filament\Resources\ModuleResource\RelationManagers;
+use App\Models\Module;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ModuleResource extends Resource
+{
+    protected static ?string $model = Module::class;
+    protected static ?string $navigationGroup = 'Academic content';
+
+    protected static ?string $navigationIcon = 'bx-book';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('course_id')
+                    ->relationship('course', 'title')
+                    ->required(),
+                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\TextInput::make('slug')->required(),
+                Forms\Components\Textarea::make('description'),
+                Forms\Components\TextInput::make('order')->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('course.title')->label('Course'),
+                Tables\Columns\TextColumn::make('order'),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListModules::route('/'),
+            'create' => Pages\CreateModule::route('/create'),
+            'edit' => Pages\EditModule::route('/{record}/edit'),
+        ];
+    }
+}
