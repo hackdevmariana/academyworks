79558a1f0666dc94f22d09c4df60a4bbd0d5e1d0
fernando <fernando@juandemariana.org>
2025-01-05 07:54:20 +0100

    Fix Course model for Filament use

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'subtitle',
+        'abstract',
+        'description',
+        'image',
+        'image_url',
+        'banner',
+        'banner_url',
+        'duration',
+        'level',
+        'language',
+        ];
 }
