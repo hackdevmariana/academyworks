c0a067690897d47f2b625779de1bb8f0e1acda35
fernando <fernando@juandemariana.org>
2025-01-11 02:34:14 +0100

    Fix Reading controller

--- app/Http/Controllers/ReadingController.php
+++ app/Http/Controllers/ReadingController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reading;
+use Illuminate\Http\Request;
 
 class ReadingController extends Controller
 {
+    public function index()
+    {
+        // Obtiene todas las lecturas con sus autores
+        $readings = Reading::with('author')->get();
+
+        return view('readings.index', compact('readings'));
+    }
+
     public function show($slug)
     {
-        $reading = Reading::with('author', 'parts')->where('slug', $slug)->firstOrFail();
+        // Encuentra una lectura por su slug y carga sus partes
+        $reading = Reading::with(['parts', 'author'])->where('slug', $slug)->firstOrFail();
+
         return view('readings.show', compact('reading'));
     }
 }
